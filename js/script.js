0905f482c467caec9ca0939b294a8cb27bac902e
gui <148351859+guibillie@users.noreply.github.com>
2023-11-08 10:55:09 -0300

    atualizado
    hgfcmhfgv

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+const btnNovo = document.querySelector(".btnNovo");
+const tela = document.querySelector(".areaNovoProduto");
+const bloqueio  = document.querySelector(".block")
+
 function verificarSituacaoEstoque(quantidade)
 {
     let quantidadeProduto = Number(quantidade);
@@ -11,8 +15,6 @@ function verificarSituacaoEstoque(quantidade)
     }
 }
 
-/* Código sem bloco de execução é executado imediatamente */
-
 const linhasProdutos = document.querySelectorAll('.linha-produto');
 
 
@@ -25,7 +27,64 @@ for (let i = 0; i < linhasProdutos.length; i++) {
     
 }
 
-const botaoNovo = document.querySelector('click', function(e){
-    e.preventdefault();
+const botaonovo = document.query
 
-});
+function ClicouBotaoAdicionar(event)
+{
+	event.preventDefault();
+
+	tela.classList.remove("escondido");
+	bloqueio.classList.remove("escondido");
+}
+
+btnNovo.addEventListener("click", ClicouBotaoAdicionar);
+
+
+const bntSalvar = document.querySelector(".botaoSalvar")
+bntSalvar.addEventListener('click', adicionarNovoProduto)
+
+
+function adicionarNovoProduto(event)
+{
+    event.preventDefault
+
+    let nome = txtNome.value;
+    let quantidade = txtQtd.value;
+    let valor =  txtValor.value;
+
+    if( nome == "")
+    {
+        console.log("Nome inválido!!");
+        return;
+    }
+    else
+    {
+        console.log(nome)
+    }
+
+    if( quantidade == "")
+    {
+        console.log("Quantidade Inválida!!");
+        return;
+    }
+    else
+    {
+        console.log(quantidade)
+    }
+
+
+    if( valor == "")
+    {
+        console.log("Valor inválido!!");
+        return;
+    }
+    else
+    {
+        console.log(valor)
+    }
+}
+
+const bntSalvar = document.querySelector(".botaoSalvar")
+bntSalvar.addEventListener('click', adicionarNovoProduto)
+
+
